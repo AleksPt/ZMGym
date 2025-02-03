48b7186207a6671745bebb2648101d9d8e466da2
Alex Petrenko <alex.pt@ya.ru>
2025-02-03 10:06:33 +0300

    Initial Commit

---
+++ Plinko/PlinkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlinkoApp.swift
+//  Plinko
+//
+//  Created by Алексей on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PlinkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
